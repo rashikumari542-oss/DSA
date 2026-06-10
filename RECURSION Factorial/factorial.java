396e9304179341f3761f84fa5e9a250e4222c626
Rashi <rashikumari542@gmail.com>
2026-06-10 15:22:09 +0530

    Add Java implementations for factorial and Fibonacci algorithms

---
+++ RECURSION Factorial/factorial.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class factorial{
+    static int f(int n){
+        if (n==0 || n==1){
+            return 1;
+        }
+        return n * f(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        int n = sc.nextInt();
+        System.out.println(f(n));
+        sc.close();
+
+    }
+}
